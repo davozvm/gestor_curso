7a44986b12f372b35f150e2f0789dd67407aed69
davozvm <dvalencia.ezzquad.com>
2026-01-29 17:13:27 -0600

    [REFACTOR] renombrar sesion_id a sesion_ids siguiendo estándares de Odoo

--- models/cursos.py
+++ models/cursos.py
@@ -3,10 +3,10 @@ from odoo import models, fields
 
 class GestorCurso(models.Model):
     _name ='gestor.curso'
-    _description ='Cursos'
+    _description ='Curso'
     
     name= fields.Char(string ='Nombre del curso', required= True)
     description= fields.Text(string= 'Descripción')
     responsable_id= fields.Many2one('res.users', string= 'Responsable', required= True)
     
-    sesion_id= fields.One2many('gestor.sesion', 'course_id', string= 'Sesiones')
+    sesion_ids= fields.One2many('gestor.sesion', 'course_id', string= 'Sesiones')
